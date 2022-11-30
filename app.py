1c3eae3f03a9e7a035d79e7a75568ee4f9734c52
Daniel Tressel <dhtressel@gmail.com>
2022-11-29 22:38:06 -0500

    initial setup

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, render_template, redirect, session
+
+from models import db, connect_db
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///jobalyze'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+connect_db(app)
