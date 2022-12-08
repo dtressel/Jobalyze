e4abce322b50f4e7915147dfbce6792e32eb1fd9
Daniel Tressel <dhtressel@gmail.com>
2022-12-08 10:14:01 -0500

    re-organized user icon popup js

--- static/js/user_icon_popup.js
+++ static/js/user_icon_popup.js
@@ -1,8 +1,13 @@
+// Load DOM objects:
 const userIcon = document.getElementById('user-icon');
 const userIconPopup = document.getElementById('user-icon-popup');
-userIcon.addEventListener('click', displayUserIconPopup);
+
 let userIconDisplayed = false;
 
+// Event Listener:
+userIcon.addEventListener('click', displayUserIconPopup);
+
+// On click of user icon display popup if not already showing
 function displayUserIconPopup(evt) {
   userIconPopup.classList.toggle('display-none');
   userIconDisplayed = !userIconDisplayed;
@@ -11,12 +16,16 @@ function displayUserIconPopup(evt) {
   }
 }
 
+// When mouse leaves user icon while popup is showing add window click event listener
 function leaveUserIcon() {
   if (userIconDisplayed) {
     window.addEventListener('click', hideUserIconPopup, {once: true});
   }
 }
 
+// When click on anywhere in the window close popup
+// except when clicking on empty space in popup
+// in that case re-add window click event listener when mouse moves
 function hideUserIconPopup(evt) {
   if (evt.target.id != "user-icon-popup") {
     userIconPopup.classList.add('display-none');
