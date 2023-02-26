2760a51b3c5a6b3e40bd3f12057252e5357a32cd
Daniel Tressel <dhtressel@gmail.com>
2023-02-25 22:53:01 -0500

    replace postgres with postgresql for heroku

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///jobalyze'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql:///jobalyze').replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
